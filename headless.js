bdc60dfdcb2f858948b6c7b23457c0ae41e8cc26
Sulka Haro <sulka@sulka.net>
2021-02-08 11:39:56 +0200

    Add nyc to test

--- headless.js
+++ headless.js
@@ -6,7 +6,7 @@ const benv = require('benv');
 const t = Date.now();
 
 benv.setup(function() {
-  benv.require(__dirname + '/bundle.report.js');
+  benv.require(__dirname + '/bundle.app.js');
 });
 
 console.log('benv require took', Date.now() -t, 'milliseconds');
